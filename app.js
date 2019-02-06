2e3428011b8e9a7b12b41e86ed0396b3ff1a0bbf
Roman Martseniuk <roma160498@yandex.ru>
2019-02-07 01:02:08 +0300

    Add "authenticationMiddleware" for each route. Now "401" status will be returned after responces for each route when user is not authorized.

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding");
     next();
 });
-app.use(router);
+app.use(authenticationMiddleware(), router);
 
 passport.use(new LocalStrategy({
     username: '',
